b956828dac35940c24aaf9dd6fdebaf074fd8078
Richard Cho <richardcho100@gmail.com>
2019-05-10 17:02:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name]
+    @reversed_name = params[:name].reverse!
   end
 
 end
